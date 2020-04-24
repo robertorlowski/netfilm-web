e4d7c61f3fef57719359e7dd0e15fd40e93c134a
Robert <robert@rozumo.pl>
2020-04-24 23:28:14 +0200

    functions

---
+++ src/lambda/consts.js
@@ -0,0 +1,2 @@
+export const mongodb_url =
+  "mongodb://mfilm:Proste123!@ds229438.mlab.com:29438/mfilm";
--- src/video2.js
+++
@@ -1,9 +0,0 @@
-var obj = {
-  name: "Peter",
-  age: 22,
-  country: "United States",
-};
-
-// Converting JS object to JSON string
-var json = JSON.stringify(obj);
-document.write(json);
